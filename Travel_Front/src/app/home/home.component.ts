7512da3e1b4cd4903b83fcd3c4f588d13781c02e
S.ELBOUAZZAOUI <s.elbouazzaoui@berger-levrault.com>
2025-06-17 12:37:37 +0200

    fixes

--- Travel_Front/src/app/home/home.component.ts
+++ Travel_Front/src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ButtonBookingComponent} from '../../lib/button-booking/button-booking.component';
+import {RouterModule} from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   imports: [
-    ButtonBookingComponent
+    ButtonBookingComponent,
+    RouterModule
 
   ],
   templateUrl: './home.component.html',
