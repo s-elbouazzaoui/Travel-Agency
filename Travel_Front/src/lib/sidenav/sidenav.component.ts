000823d6e5ba97041bf593994a98d89b7904601e
S.ELBOUAZZAOUI <s.elbouazzaoui@berger-levrault.com>
2025-06-18 20:47:52 +0200

    fixing sidenav

--- Travel_Front/src/lib/sidenav/sidenav.component.ts
+++ Travel_Front/src/lib/sidenav/sidenav.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   imports: [
+    RouterLink
   ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
